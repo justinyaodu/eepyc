96b15339baf9c0a651d9a75afa7a25197967966d
Justin Yao Du <justinyaodu@gmail.com>
2020-06-06 16:06:49 -0700

    Add initial batch of tests

---
+++ test_eepyc.py
@@ -0,0 +1,41 @@
+import unittest
+
+from eepyc import Evaluator
+
+
+class TestEepyc(unittest.TestCase):
+    def test_expression(self):
+        e = Evaluator()
+        self.assertEqual(e.eval_tags("{{ 1 + 2 }}"), "3")
+
+    def test_statement(self):
+        e = Evaluator()
+        self.assertEqual(e.eval_tags("{{% x = 7 }}{{ x }}"), "7")
+
+    def test_statement_print(self):
+        e = Evaluator()
+        self.assertEqual(e.eval_tags("{{% print('hello') }}"), "hello")
+
+    def test_newline_strip(self):
+        e = Evaluator()
+        self.assertEqual(e.eval_tags("\n\n\t{{- 9 -}}\n\n"), "\n\t9\n")
+
+    def test_newline_strip_too_many(self):
+        e = Evaluator()
+        self.assertEqual(e.eval_tags("\n\n\t{{--- 9 ---}}\n\n"), "\t9")
+
+    def test_indent(self):
+        e = Evaluator()
+        self.assertEqual(e.eval_tags("\n\t{{ [1, 2] }}"), "\n\t1\n\t2")
+
+    def test_indent_without_preceding_newline(self):
+        e = Evaluator()
+        self.assertEqual(e.eval_tags("\t{{ [1, 2] }}"), "\t1\n\t2")
+
+    def test_no_indent(self):
+        e = Evaluator()
+        self.assertEqual(e.eval_tags("\n\t{{-^ 6 }}"), "6")
+
+    def test_expression_list(self):
+        e = Evaluator()
+        self.assertEqual(e.eval_tags("{{ ['apple', 'banana'] }}"), "apple\nbanana")
